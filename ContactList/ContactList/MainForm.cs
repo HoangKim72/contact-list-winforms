dd421efc4cb3ddf148ad86c0194e235e433eeac7
HoangKim72 <hoangtv2009@gmail.com>
2025-05-27 08:33:07 +0700

    Khoi tao du an Winform voi MainFrom

---
+++ ContactList/ContactList/MainForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactList
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThemlienhe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bạn đã nhấn Thêm liên hệ!");
+        }
+    }
+}
